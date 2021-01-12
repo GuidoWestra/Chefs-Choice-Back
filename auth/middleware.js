2699630c40b9a0d550c313db886979701dbbcd75
Guido Westra <westra.guido@gmail.com>
2021-01-13 00:11:46 +0100

    Took a pause form writing middleware realised i might not need it for current userStories!

--- auth/middleware.js
+++ auth/middleware.js
@@ -1,12 +1,20 @@
-const User = require("../models").user;
-const { toData } = require("./jwt");
+// const User = require("../models").user;
+// const { toData } = require("./jwt");
 
-async function auth(req, res, next) {
-  const auth = req.headers.authorization && req.headers.authorization.split(" ");
+// async function auth(req, res, next) {
+//   const auth = req.headers.authorization && req.headers.authorization.split(" ");
 
-  if (!auth || !(auth[0] === "Bearer" || !auth[1])) {
-    return res.status(401).send({
-      message: "This endpoint is portected and requires an authorization header",
-    });
-  }
-}
+//   if (!auth || !(auth[0] === "Bearer" || !auth[1])) {
+//     return res.status(401).send({
+//       message: "This endpoint is portected and requires an authorization header",
+//     });
+//   }
+
+//   try{
+//     const data = toData(auth[1]);
+//     const user = findByPk(data.userId)
+//     if (!user) {
+//       return res.status(404).send({message})
+//     }
+//   }
+// }
