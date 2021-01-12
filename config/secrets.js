572a1d51b909d350cc7f34725c4c2d67c75bab7a
Guido Westra <westra.guido@gmail.com>
2021-01-12 18:49:15 +0100

    Made up secret jwt phrase

--- config/secrets.js
+++ config/secrets.js
@@ -1,4 +1,4 @@
 module.exports = {
-  jwtSecret: process.env.JWT_SECRET || "",
+  jwtSecret: process.env.JWT_SECRET || "e9rp^&^**@9sejg)DSUA)jpfds83*4jdsfn,*",
   API_KEY: "3e01ce49b03e4eabbeaae3fd53764503",
 };
