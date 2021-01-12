f597287cee784cd7aedafc337a1df4e0563bbc30
Guido Westra <westra.guido@gmail.com>
2021-01-12 22:52:59 +0100

    Finalized Sign Up Route!

--- index.js
+++ index.js
@@ -2,14 +2,33 @@ const express = require("express");
 const User = require("./models").user;
 const app = express();
 const { PORT } = require("./config/constants");
+const authRouter = require("./routers/auth");
+const loggerMiddleWare = require("morgan");
+const BodyParser = express.json();
+const cors = require("cors");
+
+//MiddleWares:
+//- Morgan middleware
+//  Allows for visualisation of request
+//- Body Parser
+//  Allows reading reuquest bodies of JSON Requests.
+//- Cors "Cross Origin Resource Sharing"
+//  Enables other domains to access the database
+app.use(loggerMiddleWare("dev"));
+app.use(BodyParser);
+app.use(cors());
+
+//Routers:
+//- Sign up && Logging in
+//  A endpoint set up for handling request
+//  conserning logging in and signing up
+app.use("/", authRouter);
 
 app.listen(PORT, () =>
   console.log(`
 server started on: ${PORT}
 available endpoints:
-- / Get ""
-- / Post ""
-- / Delete ""
-- / Patch ""
-`)
+- / POST 
+  - sign up
+  - log in`)
 );
---
+++ routers/auth.js
@@ -0,0 +1,36 @@
+const bcrypt = require("bcrypt");
+const { Router } = require("express");
+const { toJWT } = require("../auth/jwt");
+// const authMiddleware = require("../auth/middleware");
+const User = require("../models/").user;
+const { SALT_ROUNDS } = require("../config/constants");
+
+const router = new Router();
+
+router.post("/signup", async (req, res, next) => {
+  const { email, password, name } = req.body;
+  console.log("LOOOOK FOR MEEEEEEEEEEEEEEE :D", req.body);
+
+  if (!email || !password || !name) {
+    return res.status(400).send("Please provide an Email, password and name");
+  }
+
+  try {
+    const newUser = await User.create({
+      email,
+      password: bcrypt.hashSync(password, SALT_ROUNDS),
+      name,
+    });
+
+    delete newUser.dataValues["password"];
+
+    res.status(201).json({ userId: newUser.id });
+  } catch (e) {
+    if (e.name === "SequelizeUniqueConstraintError") {
+      return res.status(400).send({ message: "User with given credentials already present" });
+    } else {
+      return res.status(400).send({ message: "Something went wrong!" });
+    }
+  }
+});
+module.exports = router;
